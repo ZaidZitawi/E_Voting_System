2fd9cbe1c070cbd25cd41c8f5d9bc10928b2406a
Zaid Zitawi <111902956+ZaidZitawi@users.noreply.github.com>
2024-10-07 13:24:55 +0300

    Initial commit

---
+++ src/test/java/com/example/e_voting_system/EVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_voting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVotingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
